f2dde3f6a7abe8de2e390d4ef1a05a5702ff85b9
hari <hari@inspiron3420.com>
2022-06-11 14:15:47 +0800

    2.35

--- ex2.35.scm
+++ ex2.35.scm
@@ -7,4 +7,12 @@
 (define tree (cons (list 1 2) (list 3 4)))
 
 (define (count-leaves t)
-  (accu () 0 (map () ())))
+  (accu +
+        0
+        (map (lambda (sub-tree)
+                (if (pair? sub-tree)
+                    (count-leaves sub-tree)
+                    1))
+            t)))
+
+(count-leaves tree)
